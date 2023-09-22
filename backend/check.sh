b7ca4c00131039b64e92d3c44a4ff0d47eef45b9
wayangalihpratama <wgprtm@gmail.com>
2023-09-22 15:25:40 +0800

    [#2] Setup BE test

--- backend/check.sh
+++ backend/check.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+echo "Migrating main schema"
+alembic upgrade head
+
+echo "Running tests"
+COVERAGE_PROCESS_START=./.coveragerc \
+  coverage run --parallel-mode --concurrency=thread,gevent --rcfile=./.coveragerc \
+  /usr/local/bin/pytest -vvv -rP
+
+echo "Coverage"
+coverage combine --rcfile=./.coveragerc
+coverage report -m --rcfile=./.coveragerc
+
+if [[ -n "${COVERALLS_REPO_TOKEN:-}" ]] ; then
+  coveralls
+fi
+
 flake8
