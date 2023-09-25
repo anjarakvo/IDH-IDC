638d6296c563aaef41278e36476ca8523d44986d
wayangalihpratama <wgprtm@gmail.com>
2023-09-25 13:54:54 +0800

    [#2] Fix App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, waitFor } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
 test("it renders correctly", async () => {
-  const { getByTestId } = render(<App />);
+  const { getByTestId } = render(
+    <Router>
+      <App />
+    </Router>
+  );
   waitFor(() => {
     expect(getByTestId("hello")).toBeInTheDocument();
   });
