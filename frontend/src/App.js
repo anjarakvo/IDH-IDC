2169da28487234e39063c227655bd2936c7501f7
wayangalihpratama <wgprtm@gmail.com>
2023-09-26 17:46:48 +0800

    [#4] Init PrivateRoutes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages/home";
+import { PrivateRoutes } from "./components/route";
 import { PageLayout } from "./components/layout";
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
+import { Dashboard } from "./pages/dashboard";
 
 const App = () => {
   return (
     <PageLayout testid="page-layout">
       <Routes>
+        <Route element={<PrivateRoutes />}>
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
       </Routes>
     </PageLayout>
   );
