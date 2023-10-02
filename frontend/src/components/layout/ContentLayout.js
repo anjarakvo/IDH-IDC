2aefbad23c8920e8364bf5ad89aba3b6e4ce3fb1
wayangalihpratama <wgprtm@gmail.com>
2023-10-02 18:31:14 +0800

    [#8] Add page title above content layout

--- frontend/src/components/layout/ContentLayout.js
+++ frontend/src/components/layout/ContentLayout.js
@@ -1,16 +1,43 @@
 import React from "react";
-import { Breadcrumb } from "antd";
+import { Breadcrumb, Card } from "antd";
 
-const ContentLayout = ({ children, breadcrumbItems = [] }) => {
+const ContentLayout = ({
+  children,
+  breadcrumbItems = [],
+  title = null,
+  subTitle = null,
+}) => {
   const hasBreadcrumb = breadcrumbItems.length;
+  const renderCard = hasBreadcrumb || title;
+
+  if (!renderCard) {
+    return <div className="content-wrapper">{children}</div>;
+  }
+
   return (
     <div>
-      {hasBreadcrumb ? (
-        <Breadcrumb style={{ margin: "16px 0" }} items={breadcrumbItems} />
-      ) : (
-        ""
-      )}
-      <div>{children}</div>
+      <Card style={{ padding: "12px 0 8px 0" }}>
+        {hasBreadcrumb ? (
+          <Breadcrumb testid="breadcrumb" items={breadcrumbItems} />
+        ) : (
+          ""
+        )}
+        {title ? (
+          <div testid="title" className="title">
+            Page Title
+          </div>
+        ) : (
+          ""
+        )}
+        {subTitle ? (
+          <div testid="subTitle" className="subTitle">
+            {subTitle}
+          </div>
+        ) : (
+          ""
+        )}
+      </Card>
+      <div className="content-wrapper">{children}</div>
     </div>
   );
 };
--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Card } from "antd";
 import { ContentLayout } from "../../components/layout";
 
 const Home = () => {
   return (
-    <ContentLayout breadcrumbItems={[{ title: "Home" }]}>
-      <h1>Home</h1>
+    <ContentLayout
+      breadcrumbItems={[{ title: "Home" }]}
+      title="HomePage"
+      subTitle="Lorem ipsum sit dolor"
+    >
+      <Card>Test1</Card>
+      <Card>Test2</Card>
     </ContentLayout>
   );
 };
