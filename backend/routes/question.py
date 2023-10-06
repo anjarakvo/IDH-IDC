2b09b90995e714af746876ba783ad99891670c6a
wayangalihpratama <wgprtm@gmail.com>
2023-10-06 19:20:45 +0800

    [#18] Create question by crops endpoint

---
+++ backend/routes/question.py
@@ -0,0 +1,37 @@
+import db.crud_question as crud_question
+
+from fastapi import (
+    APIRouter, Request, Depends, Body
+)
+from fastapi.security import (
+    HTTPBearer, HTTPBasicCredentials as credentials
+)
+from sqlalchemy.orm import Session
+from typing import List
+
+from db.connection import get_session
+from models.question import (
+    QuestionGroupParam, QuestionGroupListDict
+)
+
+security = HTTPBearer()
+question_route = APIRouter()
+
+
+@question_route.post(
+    "/questions",
+    response_model=List[QuestionGroupListDict],
+    summary="get question by crop ID",
+    name="question:get_by_crops",
+    tags=["Question"]
+)
+def get_question_by_crop_id(
+    req: Request,
+    payload: List[QuestionGroupParam] = Body(),
+    session: Session = Depends(get_session),
+    credentials: credentials = Depends(security)
+):
+    questions = crud_question.get_question_by_crop(
+        session=session, params=payload
+    )
+    return questions
---
+++ backend/tests/test_030_question.py
@@ -0,0 +1,112 @@
+import sys
+import pytest
+
+from fastapi import FastAPI
+from httpx import AsyncClient
+from sqlalchemy.orm import Session
+from tests.test_000_main import Acc
+
+sys.path.append("..")
+
+non_admin_account = Acc(email="support@akvo.org", token=None)
+admin_account = Acc(email="admin@akvo.org", token=None)
+
+
+class TestQuestionRoute():
+    @pytest.mark.asyncio
+    async def test_get_question_by_crop_ID(
+        self, app: FastAPI, session: Session, client: AsyncClient
+    ) -> None:
+        # without cred
+        res = await client.post(app.url_path_for("question:get_by_crops"))
+        assert res.status_code == 403
+        # with non admin cred
+        res = await client.post(
+            app.url_path_for("question:get_by_crops"),
+            json=[{
+                "crop": 1,
+                "breakdown": False,
+            }, {
+                "crop": 2,
+                "breakdown": True,
+            }],
+            headers={"Authorization": f"Bearer {non_admin_account.token}"},
+        )
+        assert res.status_code == 200
+        # test valid search
+        res = await client.post(
+            app.url_path_for("question:get_by_crops"),
+            json=[{
+                "crop": 1,
+                "breakdown": False,
+            }, {
+                "crop": 2,
+                "breakdown": True,
+            }],
+            headers={"Authorization": f"Bearer {admin_account.token}"},
+        )
+        assert res.status_code == 200
+        res = res.json()
+        assert res == [{
+            'crop_id': 1,
+            'crop_name': 'Wheat',
+            'questions': [{
+                'id': 1,
+                'parent': None,
+                'code': 'Q1',
+                'text': 'Net Income per day',
+                'description': None,
+                'default_value': 'function() { return #Q2 * #Q3 / 30; }',
+                'created_by': 1,
+                'childrens': []
+            }, {
+                'id': 2,
+                'parent': None,
+                'code': 'Q2',
+                'text': 'Income from Crop / Month',
+                'description': None,
+                'default_value': None,
+                'created_by': 1,
+                'childrens': []
+            }, {
+                'id': 3,
+                'parent': None,
+                'code': 'Q3',
+                'text': 'Cost of Production / Month',
+                'description': None,
+                'default_value': None,
+                'created_by': 1,
+                'childrens': []
+            }]
+        }, {
+            'crop_id': 2,
+            'crop_name': 'Rice',
+            'questions': [{
+                'id': 1,
+                'parent': None,
+                'code': 'Q1',
+                'text': 'Net Income per day',
+                'description': None,
+                'default_value': 'function() { return #Q2 * #Q3 / 30; }',
+                'created_by': 1,
+                'childrens': []
+            }, {
+                'id': 2,
+                'parent': None,
+                'code': 'Q2',
+                'text': 'Income from Crop / Month',
+                'description': None,
+                'default_value': None,
+                'created_by': 1,
+                'childrens': []
+            }, {
+                'id': 3,
+                'parent': None,
+                'code': 'Q3',
+                'text': 'Cost of Production / Month',
+                'description': None,
+                'default_value': None,
+                'created_by': 1,
+                'childrens': []
+            }]
+        }]
