9188f9f9ba33affe326837c6109d9eb76c73a9b1
wayangalihpratama <wgprtm@gmail.com>
2023-10-17 13:34:02 +0800

    [#28] Initial welcome page style & render page title, subtitle

--- frontend/src/pages/welcome/Welcome.js
+++ frontend/src/pages/welcome/Welcome.js
@@ -1,8 +1,33 @@
 import React from "react";
 import "./welcome.scss";
+import { Row, Col } from "antd";
 
 const Welcome = () => {
-  return <h1>Welcome</h1>;
+  return (
+    <Row className="welcome-container">
+      <div className="right-clip-path-wrapper" />
+      <Col span={24} className="welcome-content-wrapper">
+        {/* Page Title */}
+        <h1 data-testid="page-title" className="page-title">
+          Welcome to the IDC
+        </h1>
+        <h3 data-testid="page-subtitle" className="page-subtitle">
+          The income driver calculator version 2.0 is an analytics tool that
+          uses both Living Income and Better Income to generate scenarios based
+          off changes in different income dirvers.
+        </h3>
+        {/* EOL Page Title */}
+
+        {/* Card Menu */}
+        <Row className="card-menu-wrapper">
+          <Col span={24}>1</Col>
+          <Col span={24}>2</Col>
+          <Col span={24}>3</Col>
+        </Row>
+        {/* EOL Card Menu */}
+      </Col>
+    </Row>
+  );
 };
 
 export default Welcome;
--- frontend/src/pages/welcome/__tests__/Welcome.test.js
+++ frontend/src/pages/welcome/__tests__/Welcome.test.js
@@ -1,9 +1,30 @@
-it.todo("should render navigation menu for non admin user");
-it.todo("should render navigation menu for dmin user");
-it.todo("should render welcome title and subtitle");
-it.todo("should render cases and explore studies card menu for all user role");
-it.todo("should render admin card menu only for super admin and admin role");
-it.todo("should go to about IDC page if About IDC menu clicked");
-it.todo("should go to cases page if Cases menu clicked");
-it.todo("should go to explore studies page if Explore Studies menu clicked");
-it.todo("should go to admin page if Admin menu clicked");
+import { render, waitFor, fireEvent } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { PageLayout } from "../../../components/layout";
+import Welcome from "../Welcome";
+
+describe("Welcome page", () => {
+  it.todo("should render navigation menu for non admin user");
+  it.todo("should render navigation menu for admin user");
+
+  it("should render welcome title and subtitle", () => {
+    const { getByTestId } = render(
+      <Router>
+        <PageLayout>
+          <Welcome />
+        </PageLayout>
+      </Router>
+    );
+    expect(getByTestId("page-title")).toBeInTheDocument();
+    expect(getByTestId("page-subtitle")).toBeInTheDocument();
+  });
+
+  it.todo(
+    "should render cases and explore studies card menu for all user role"
+  );
+  it.todo("should render admin card menu only for super admin and admin role");
+  it.todo("should go to about IDC page if About IDC menu clicked");
+  it.todo("should go to cases page if Cases menu clicked");
+  it.todo("should go to explore studies page if Explore Studies menu clicked");
+  it.todo("should go to admin page if Admin menu clicked");
+});
