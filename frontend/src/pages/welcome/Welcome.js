9fc21feed4ab25f2e2a9379a881ee2781aca006a
wayangalihpratama <wgprtm@gmail.com>
2023-10-17 16:44:38 +0800

    [#28] Render admin card menu only for user with admin role

--- frontend/src/pages/welcome/Welcome.js
+++ frontend/src/pages/welcome/Welcome.js
@@ -4,6 +4,10 @@ import { Row, Col, Card, Button, Image } from "antd";
 import IconCases from "../../assets/icons/icon-cases.png";
 import IconExploreStudies from "../../assets/icons/icon-explore-studies.png";
 import IconAdmin from "../../assets/icons/icon-admin.png";
+import { UserState } from "../../store";
+
+const adminRole = ["super_admin", "admin"];
+const allUserRole = [...adminRole, "editor", "viewer", "user"];
 
 const cardMenus = [
   {
@@ -13,7 +17,7 @@ const cardMenus = [
       "Data Entry is an important interactive way to retrieve information of objects since users will frequently add, change or delete information.",
     icon: IconCases,
     path: null,
-    role: null,
+    role: allUserRole,
   },
   {
     testid: "card-menu-explore-studies",
@@ -22,7 +26,7 @@ const cardMenus = [
       "Data Entry is an important interactive way to retrieve information of objects since users will frequently add, change or delete information.",
     icon: IconExploreStudies,
     path: null,
-    role: null,
+    role: allUserRole,
   },
   {
     testid: "card-menu-admin",
@@ -31,11 +35,13 @@ const cardMenus = [
       "Data Entry is an important interactive way to retrieve information of objects since users will frequently add, change or delete information.",
     icon: IconAdmin,
     path: null,
-    role: ["super_admin", "admin"],
+    role: adminRole,
   },
 ];
 
 const Welcome = () => {
+  const userRole = UserState.useState((s) => s.role);
+
   return (
     <Row className="welcome-container">
       <div className="right-clip-path-wrapper" />
@@ -47,48 +53,49 @@ const Welcome = () => {
         <h3 data-testid="page-subtitle" className="page-subtitle">
           The income driver calculator version 2.0 is an analytics tool that
           uses both Living Income and Better Income to generate scenarios based
-          off changes in different income dirvers.
+          off changes in different income drivers.
         </h3>
         {/* EOL Page Title */}
 
         {/* Card Menu */}
         <Row className="card-menu-wrapper" gutter={[14, 14]}>
-          {cardMenus.map((cm, cmi) => {
-            return (
-              <Col key={`card-menu-${cmi}`} span={24} data-testid={cm.testid}>
-                <Card className="card-container">
-                  <Row
-                    justify="center"
-                    align="middle"
-                    justifyContent="center"
-                    className="card-content-wrapper"
-                  >
-                    <Col span={2} align="center" className="icon-wrapper">
-                      <Image
-                        src={cm.icon}
-                        preview={false}
-                        data-testid={`${cm.testid}-icon`}
-                      />
-                    </Col>
-                    <Col span={20} className="text-wrapper">
-                      <h4 data-testid={`${cm.testid}-name`}>{cm.name}</h4>
-                      <p data-testid={`${cm.testid}-description`}>
-                        {cm.description}
-                      </p>
-                    </Col>
-                    <Col span={2} className="button-wrapper">
-                      <Button
-                        data-testid={`${cm.testid}-button`}
-                        className="button-green"
-                      >
-                        Explore
-                      </Button>
-                    </Col>
-                  </Row>
-                </Card>
-              </Col>
-            );
-          })}
+          {cardMenus
+            .filter((cm) => cm.role.includes(userRole))
+            .map((cm, cmi) => {
+              return (
+                <Col key={`card-menu-${cmi}`} span={24} data-testid={cm.testid}>
+                  <Card className="card-container">
+                    <Row
+                      justify="center"
+                      align="middle"
+                      className="card-content-wrapper"
+                    >
+                      <Col span={2} align="center" className="icon-wrapper">
+                        <Image
+                          src={cm.icon}
+                          preview={false}
+                          data-testid={`${cm.testid}-icon`}
+                        />
+                      </Col>
+                      <Col span={20} className="text-wrapper">
+                        <h4 data-testid={`${cm.testid}-name`}>{cm.name}</h4>
+                        <p data-testid={`${cm.testid}-description`}>
+                          {cm.description}
+                        </p>
+                      </Col>
+                      <Col span={2} className="button-wrapper">
+                        <Button
+                          data-testid={`${cm.testid}-button`}
+                          className="button-green"
+                        >
+                          Explore
+                        </Button>
+                      </Col>
+                    </Row>
+                  </Card>
+                </Col>
+              );
+            })}
         </Row>
         {/* EOL Card Menu */}
       </Col>
--- frontend/src/pages/welcome/__tests__/Welcome.test.js
+++ frontend/src/pages/welcome/__tests__/Welcome.test.js
@@ -1,7 +1,20 @@
-import { render, waitFor, fireEvent } from "@testing-library/react";
+import { render, waitFor, fireEvent, act } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { PageLayout } from "../../../components/layout";
 import Welcome from "../Welcome";
+import { UserState } from "../../../store";
+
+const organisation_detail = {
+  id: 1,
+  name: "Akvo",
+};
+const business_unit_detail = [
+  {
+    id: 1,
+    name: "Meat Guy",
+    role: "admin",
+  },
+];
 
 describe("Welcome page", () => {
   it.todo("should render navigation menu for non admin user");
@@ -20,13 +33,28 @@ describe("Welcome page", () => {
   });
 
   it("should render cases and explore studies card menu for all user role", () => {
-    const { getByTestId } = render(
+    const { getByTestId, queryByTestId } = render(
       <Router>
         <PageLayout>
           <Welcome />
         </PageLayout>
       </Router>
     );
+
+    act(() => {
+      UserState.update((s) => {
+        s.id = 1;
+        s.fullname = "John Doe";
+        s.email = "editor@akvo.com";
+        s.role = "editor";
+        s.active = true;
+        s.organisation_detail = organisation_detail;
+        s.business_unit_detail = business_unit_detail;
+        s.tags_count = 2;
+        s.cases_count = 1;
+      });
+    });
+
     expect(getByTestId("card-menu-cases")).toBeInTheDocument();
     expect(getByTestId("card-menu-cases-icon")).toBeInTheDocument();
     expect(getByTestId("card-menu-cases-name")).toBeInTheDocument();
@@ -40,9 +68,54 @@ describe("Welcome page", () => {
       getByTestId("card-menu-explore-studies-description")
     ).toBeInTheDocument();
     expect(getByTestId("card-menu-explore-studies-button")).toBeInTheDocument();
+
+    expect(queryByTestId("card-menu-admin")).not.toBeInTheDocument();
   });
 
-  it.todo("should render admin card menu only for super admin and admin role");
+  it("should render admin card menu only for super admin and admin role", () => {
+    const { getByTestId } = render(
+      <Router>
+        <PageLayout>
+          <Welcome />
+        </PageLayout>
+      </Router>
+    );
+
+    act(() => {
+      UserState.update((s) => {
+        s.id = 1;
+        s.fullname = "John Doe";
+        s.email = "admin@akvo.com";
+        s.role = "admin";
+        s.active = true;
+        s.organisation_detail = organisation_detail;
+        s.business_unit_detail = business_unit_detail;
+        s.tags_count = 2;
+        s.cases_count = 1;
+      });
+    });
+
+    expect(getByTestId("card-menu-cases")).toBeInTheDocument();
+    expect(getByTestId("card-menu-cases-icon")).toBeInTheDocument();
+    expect(getByTestId("card-menu-cases-name")).toBeInTheDocument();
+    expect(getByTestId("card-menu-cases-description")).toBeInTheDocument();
+    expect(getByTestId("card-menu-cases-button")).toBeInTheDocument();
+
+    expect(getByTestId("card-menu-explore-studies")).toBeInTheDocument();
+    expect(getByTestId("card-menu-explore-studies-icon")).toBeInTheDocument();
+    expect(getByTestId("card-menu-explore-studies-name")).toBeInTheDocument();
+    expect(
+      getByTestId("card-menu-explore-studies-description")
+    ).toBeInTheDocument();
+    expect(getByTestId("card-menu-explore-studies-button")).toBeInTheDocument();
+
+    expect(getByTestId("card-menu-admin")).toBeInTheDocument();
+    expect(getByTestId("card-menu-admin-icon")).toBeInTheDocument();
+    expect(getByTestId("card-menu-admin-name")).toBeInTheDocument();
+    expect(getByTestId("card-menu-admin-description")).toBeInTheDocument();
+    expect(getByTestId("card-menu-admin-button")).toBeInTheDocument();
+  });
+
   it.todo("should go to about IDC page if About IDC menu clicked");
   it.todo("should go to cases page if Cases menu clicked");
   it.todo("should go to explore studies page if Explore Studies menu clicked");
