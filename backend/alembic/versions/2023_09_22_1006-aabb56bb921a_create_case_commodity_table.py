aaa33865c5c2461ee0cd74355406022625dbaec1
dedenbangkit <mail@dedenbangkit.com>
2023-10-20 07:47:06 +0700

    [#41] Modify migrations, fix enum in migrations

--- backend/alembic/versions/2023_09_22_1006-aabb56bb921a_create_case_commodity_table.py
+++ backend/alembic/versions/2023_09_22_1006-aabb56bb921a_create_case_commodity_table.py
@@ -12,48 +12,48 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'aabb56bb921a'
-down_revision: Union[str, None] = '718e76de8207'
+revision: str = "aabb56bb921a"
+down_revision: Union[str, None] = "718e76de8207"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
     op.create_table(
-        'case_commodity',
-        sa.Column('id', sa.Integer(), nullable=False),
-        sa.Column('case', sa.Integer(), sa.ForeignKey('case.id')),
-        sa.Column('commodity', sa.Integer(), sa.ForeignKey('commodity.id')),
+        "case_commodity",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("case", sa.Integer(), sa.ForeignKey("case.id")),
+        sa.Column("commodity", sa.Integer()),
         sa.Column(
-            'focus_commodity', sa.SmallInteger(),
-            server_default='0', nullable=False),
-        sa.Column(
-            'breakdown', sa.SmallInteger(),
-            server_default='0', nullable=False),
-        sa.PrimaryKeyConstraint('id'),
+            "commodity_type",
+            sa.Enum(
+                "focus",
+                "secondary",
+                "tertiary",
+                "diversified",
+                name="case_commodity_type",
+            ),
+            nullable=False,
+        ),
+        sa.Column("breakdown", sa.SmallInteger(), server_default="0", nullable=False),
+        sa.Column("area_size_unit", sa.String(), nullable=False),
+        sa.Column("volume_measurement_unit", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
         sa.ForeignKeyConstraint(
-            ['case'], ['case.id'],
-            name='case_commodity_case_constraint',
-            ondelete='CASCADE'),
-        sa.ForeignKeyConstraint(
-            ['commodity'], ['commodity.id'],
-            name='case_commodity_commodity_constraint',
-            ondelete='CASCADE'),
+            ["case"],
+            ["case.id"],
+            name="case_commodity_case_constraint",
+            ondelete="CASCADE",
+        ),
         sa.UniqueConstraint(
-            'case', 'commodity',
-            name='case_commodity_unique')
+            "case", "commodity", "commodity_type", name="case_commodity_unique"
+        ),
     )
-    op.create_index(
-        op.f('ix_case_commodity_id'), 'case_commodity',
-        ['id'], unique=True)
+    op.create_index(op.f("ix_case_commodity_id"), "case_commodity", ["id"], unique=True)
 
 
 def downgrade() -> None:
-    op.drop_index(
-        op.f('ix_case_commodity_id'), table_name='case_commodity')
-    op.drop_constraint(
-        'case_commodity_unique',
-        'case_commodity',
-        type_='unique'
-    )
-    op.drop_table('case_commodity')
+    op.drop_index(op.f("ix_case_commodity_id"), table_name="case_commodity")
+    op.drop_constraint("case_commodity_unique", "case_commodity", type_="unique")
+    op.drop_table("case_commodity")
+    op.execute('DROP TYPE "case_commodity_type"')
--- backend/alembic/versions/2023_09_22_1021-ff56dba95c67_create_visualization_table.py
+++ backend/alembic/versions/2023_09_22_1021-ff56dba95c67_create_visualization_table.py
@@ -13,49 +13,48 @@ import sqlalchemy.dialects.postgresql as pg
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'ff56dba95c67'
-down_revision: Union[str, None] = '52b25021f3f8'
+revision: str = "ff56dba95c67"
+down_revision: Union[str, None] = "52b25021f3f8"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
     op.create_table(
-        'visualization',
-        sa.Column('id', sa.Integer(), nullable=False),
-        sa.Column('case', sa.Integer(), sa.ForeignKey('case.id')),
-        sa.Column('segment', sa.Integer(), sa.ForeignKey('segment.id')),
+        "visualization",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("case", sa.Integer(), sa.ForeignKey("case.id")),
+        sa.Column("segment", sa.Integer(), sa.ForeignKey("segment.id")),
         sa.Column(
-            'tab',
+            "tab",
             sa.Enum(
-                'sensitivity_analysis',
-                'scenario_modeling',
-                name='visualization_tab_enum'
+                "sensitivity_analysis", "scenario_modeling", name="visualization_tab"
             ),
-            nullable=False),
-        sa.Column('config', pg.JSONB()),
-        sa.PrimaryKeyConstraint('id'),
+            nullable=False,
+        ),
+        sa.Column("config", pg.JSONB()),
+        sa.PrimaryKeyConstraint("id"),
         sa.ForeignKeyConstraint(
-            ['case'], ['case.id'],
-            name='visualization_case_constraint',
-            ondelete='CASCADE'),
+            ["case"],
+            ["case.id"],
+            name="visualization_case_constraint",
+            ondelete="CASCADE",
+        ),
         sa.ForeignKeyConstraint(
-            ['segment'], ['segment.id'],
-            name='visualization_segment_constraint',
-            ondelete='CASCADE'),
-        sa.UniqueConstraint(
-            'segment', 'tab', name='visualization_segment_tab_unique')
+            ["segment"],
+            ["segment.id"],
+            name="visualization_segment_constraint",
+            ondelete="CASCADE",
+        ),
+        sa.UniqueConstraint("segment", "tab", name="visualization_segment_tab_unique"),
     )
-    op.create_index(
-        op.f('ix_visualization_id'), 'case', ['id'], unique=True)
+    op.create_index(op.f("ix_visualization_id"), "case", ["id"], unique=True)
 
 
 def downgrade() -> None:
-    op.drop_index(op.f('ix_visualization_id'), table_name='visualization')
+    op.drop_index(op.f("ix_visualization_id"), table_name="visualization")
     op.drop_constraint(
-        'visualization_segment_tab_unique',
-        'visualization',
-        type_='unique'
+        "visualization_segment_tab_unique", "visualization", type_="unique"
     )
-    op.drop_table('visualization')
-    op.execute('DROP TYPE visualization_tab_enum')
+    op.drop_table("visualization")
+    op.execute("DROP TYPE visualization_tab_enum")
