d44450149a0d0d97b1081edf8c66688f36480f14
dedenbangkit <mail@dedenbangkit.com>
2023-10-20 07:48:23 +0700

    [#41] Modify most of models, fix duplicated enum

--- backend/models/case_commodity.py
+++ backend/models/case_commodity.py
@@ -1,15 +1,24 @@
+import enum
 from db.connection import Base
-from sqlalchemy import Column, Integer, SmallInteger, ForeignKey, String
+from sqlalchemy import Column, Integer, SmallInteger, ForeignKey, String, Enum
 from sqlalchemy.orm import relationship
 from typing import Optional
 from pydantic import BaseModel
 from typing_extensions import TypedDict
 
 
+class CaseCommodityType(enum.Enum):
+    focus = "focus"
+    secondary = "secondary"
+    tertiary = "tertiary"
+    diversified = "diversified"
+
+
 class SimplifiedCaseCommodityDict(TypedDict):
     id: int
     commodity: int
     breakdown: bool
+    commodity_type: CaseCommodityType
     area_size_unit: str
     volume_measurement_unit: str
 
@@ -20,8 +29,11 @@ class CaseCommodity(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     case = Column(Integer, ForeignKey("case.id"))
     commodity = Column(Integer, ForeignKey("commodity.id"))
-    focus_commodity = Column(SmallInteger, nullable=False, default=0)
     breakdown = Column(SmallInteger, nullable=False, default=0)
+    commodity_type = Column(
+        Enum(CaseCommodityType, name="case_commodity_type"),
+        nullable=False,
+    )
     area_size_unit = Column(String, nullable=False)
     volume_measurement_unit = Column(String, nullable=False)
 
@@ -43,16 +55,16 @@ class CaseCommodity(Base):
         commodity: int,
         area_size_unit: str,
         volume_measurement_unit: str,
+        commodity_type: CaseCommodityType,
         case: Optional[int] = None,
         id: Optional[int] = None,
-        focus_commodity: Optional[int] = 0,
         breakdown: Optional[int] = 0,
     ):
         self.id = id
         self.case = case
         self.commodity = commodity
-        self.focus_commodity = focus_commodity
         self.breakdown = breakdown
+        self.commodity_type = commodity_type
         self.area_size_unit = area_size_unit
         self.volume_measurement_unit = volume_measurement_unit
 
@@ -65,6 +77,7 @@ class CaseCommodity(Base):
             "id": self.id,
             "commodity": self.commodity,
             "breakdown": self.breakdown,
+            "commodity_type": self.commodity_type,
             "area_size_unit": self.area_size_unit,
             "volume_measurement_unit": self.volume_measurement_unit,
         }
@@ -74,7 +87,7 @@ class CaseCommodityBase(BaseModel):
     id: int
     case: int
     commodity: int
-    focus_commodity: Optional[int] = 0
+    commodity_type: CaseCommodityType
     breakdown: Optional[int] = 0
     area_size_unit: str
     volume_measurement_unit: str
