5877eaee71dc9e5e192d239e6b00dab6a5d10283
dedenbangkit <mail@dedenbangkit.com>
2023-10-25 20:08:10 +0700

    Fix truncator when removing case table

--- backend/utils/truncator.py
+++ backend/utils/truncator.py
@@ -3,9 +3,9 @@ from sqlalchemy.sql import text
 
 
 def truncatedb(session: Session, table: str) -> None:
-    session.execute(text(f"TRUNCATE TABLE {table} CASCADE;"))
+    session.execute(text(f'TRUNCATE TABLE "{table}" CASCADE;'))
     session.execute(text(f"ALTER SEQUENCE {table}_id_seq RESTART WITH 1;"))
-    session.execute(text(f"UPDATE {table} SET id=nextval('{table}_id_seq');"))
+    session.execute(text(f"UPDATE \"{table}\" SET id=nextval('{table}_id_seq');"))
     session.commit()
     session.flush()
     return f"{table} Truncated"
