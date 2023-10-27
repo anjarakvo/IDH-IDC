84dd068e4c4b2cbac93cfd043469e80e0cee510c
wayangalihpratama <wgprtm@gmail.com>
2023-10-27 15:21:25 +0800

    [#53] Livin income bencmark source

---
+++ backend/alembic/versions/2023_10_27_0621-6a5d05824ea0_alter_living_income_benchmark_table.py
@@ -0,0 +1,100 @@
+"""alter living income benchmark table
+
+Revision ID: 6a5d05824ea0
+Revises: 94341a9d63ad
+Create Date: 2023-10-27 06:21:49.208459
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '6a5d05824ea0'
+down_revision: Union[str, None] = '94341a9d63ad'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.drop_constraint(
+        'living_income_benchmark_currency_constraint',
+        'living_income_benchmark',
+        type_='foreignkey'
+    )
+    op.drop_constraint(
+        'country_currency_unique',
+        'living_income_benchmark',
+        type_='unique'
+    )
+    op.drop_column('living_income_benchmark', 'currency')
+    op.drop_column('living_income_benchmark', 'value')
+    op.add_column(
+        'living_income_benchmark',
+        sa.Column('region', sa.Integer(), sa.ForeignKey('region.id')),
+    )
+    op.create_foreign_key(
+        'living_income_benchmark_region_constraint',
+        'living_income_benchmark',
+        'region',
+        ['region'],
+        ['id'],
+        ondelete='CASCADE'
+    )
+    op.add_column(
+        'living_income_benchmark',
+        sa.Column('household_size', sa.Float(), nullable=False),
+    )
+    op.add_column(
+        'living_income_benchmark',
+        sa.Column('source', sa.String(), nullable=True),
+    )
+    op.add_column(
+        'living_income_benchmark',
+        sa.Column('lcu', sa.Float(), nullable=False),
+    )
+    op.add_column(
+        'living_income_benchmark',
+        sa.Column('usd', sa.Float(), nullable=False),
+    )
+    op.add_column(
+        'living_income_benchmark',
+        sa.Column('eur', sa.Float(), nullable=False),
+    )
+
+
+def downgrade() -> None:
+    op.add_column(
+        'living_income_benchmark',
+        sa.Column('currency', sa.Integer(), sa.ForeignKey('currency.id')),
+    )
+    op.create_foreign_key(
+        'living_income_benchmark_currency_constraint',
+        'living_income_benchmark',
+        'currency',
+        ['currency'],
+        ['id'],
+        ondelete='CASCADE'
+    )
+    op.create_unique_constraint(
+        'country_currency_unique',
+        'living_income_benchmark',
+        ['country', 'currency']
+    )
+    op.add_column(
+        'living_income_benchmark',
+        sa.Column('value', sa.Float(), nullable=False),
+    )
+    op.drop_constraint(
+        'living_income_benchmark_region_constraint',
+        'living_income_benchmark',
+        type_='foreignkey'
+    )
+    op.drop_column('living_income_benchmark', 'region')
+    op.drop_column('living_income_benchmark', 'household_size')
+    op.drop_column('living_income_benchmark', 'source')
+    op.drop_column('living_income_benchmark', 'lcu')
+    op.drop_column('living_income_benchmark', 'usd')
+    op.drop_column('living_income_benchmark', 'eur')
--- backend/models/living_income_benchmark.py
+++ backend/models/living_income_benchmark.py
@@ -1,5 +1,5 @@
 from db.connection import Base
-from sqlalchemy import Column, Integer, Float, ForeignKey
+from sqlalchemy import Column, Integer, Float, ForeignKey, String
 from sqlalchemy.orm import relationship
 from typing import Optional
 from typing_extensions import TypedDict
@@ -9,9 +9,13 @@ from pydantic import BaseModel
 class LivingIncomeBenchmarkDict(TypedDict):
     id: int
     country: int
-    currency: int
+    region: int
+    household_size: float
     year: int
-    value: float
+    source: Optional[str]
+    lcu: float
+    usd: float
+    eur: float
 
 
 class LivingIncomeBenchmark(Base):
@@ -19,9 +23,13 @@ class LivingIncomeBenchmark(Base):
 
     id = Column(Integer, primary_key=True, nullable=False)
     country = Column(Integer, ForeignKey('country.id'))
-    currency = Column(Integer, ForeignKey('currency.id'))
+    region = Column(Integer, ForeignKey('region.id'))
+    household_size = Column(Float, nullable=False)
     year = Column(Integer, nullable=False)
-    value = Column(Float, nullable=False)
+    source = Column(String, nullable=True)
+    lcu = Column(Float, nullable=False)
+    usd = Column(Float, nullable=False)
+    eur = Column(Float, nullable=False)
 
     country_detail = relationship(
         'Country',
@@ -29,26 +37,34 @@ class LivingIncomeBenchmark(Base):
         passive_deletes=True,
         backref='country_living_income_benchmark'
     )
-    currency_detail = relationship(
-        'Currency',
+    region_detail = relationship(
+        'region',
         cascade="all, delete",
         passive_deletes=True,
-        backref='currency_living_income_benchmark'
+        backref='region_living_income_benchmark'
     )
 
     def __init__(
         self,
-        id: Optional[int],
         country: int,
-        currency: int,
+        region: int,
         year: int,
-        value: float,
+        household_size: float,
+        lcu: float,
+        usd: float,
+        eur: float,
+        id: Optional[int] = None,
+        source: Optional[str] = None,
     ):
         self.id = id
         self.country = country
-        self.currency = currency
+        self.region = region
         self.year = year
-        self.value = value
+        self.household_size = household_size
+        self.source = source
+        self.lcu = lcu
+        self.usd = usd
+        self.eur = eur
 
     def __repr__(self) -> int:
         return f"<LivingIncomeBenchmark {self.id}>"
@@ -58,15 +74,23 @@ class LivingIncomeBenchmark(Base):
         return {
             "id": self.id,
             "country": self.country,
-            "currency": self.currency,
+            "region": self.region,
             "year": self.year,
-            "value": self.value,
+            "household_size": self.household_size,
+            "source": self.source,
+            "lcu": self.lcu,
+            "usd": self.usd,
+            "eur": self.eur,
         }
 
 
 class LivingIncomeBenchmarkBase(BaseModel):
     id: int
     country: int
-    currency: int
+    region: int
     year: int
-    value: float
+    household_size: float
+    lcu: float
+    usd: float
+    eur: float
+    source: Optional[str]
