ff058033eacde084931ae7dbf52f83449e90d957
wayangalihpratama <wgprtm@gmail.com>
2023-11-03 17:56:50 +0800

    [#62] Refine regions, benchmark, and cpi seeder

--- backend/seeder/benchmark.py
+++ backend/seeder/benchmark.py
@@ -1,34 +1,50 @@
 import os
 import sys
 import pandas as pd
-from core.config import generate_config_file
 from db.connection import Base, engine, SessionLocal
 from utils.truncator import truncatedb
-from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from models.living_income_benchmark import LivingIncomeBenchmark
+from models.cpi import Cpi
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MASTER_DIR = BASE_DIR + "/source/master/"
 sys.path.append(BASE_DIR)
 
 
-def seeder_benchmark(session: Session, engine: create_engine):
-
+def seeder_benchmark(session: Session):
     ## living income benchmark
     truncatedb(session=session, table="living_income_benchmark")
     li_benchmark = pd.read_csv(MASTER_DIR + "li_benchmark.csv")
     # Filter rows where the list does not contain any string values
     filtered_lib = li_benchmark[li_benchmark["country_id"] != li_benchmark["country"]]
-    filtered_lib.drop(columns=['country', 'region'], inplace=True)
-    filtered_lib = filtered_lib.rename(columns={
-        "LCU": "lcu",
-        "USD": "usd",
-        "EUR": "eur",
-        "country_id": "country",
-        "region_id": "region",
-    })
+    filtered_lib.drop(columns=["country", "region"], inplace=True)
+    filtered_lib = filtered_lib.rename(
+        columns={
+            "LCU": "lcu",
+            "USD": "usd",
+            "EUR": "eur",
+            "country_id": "country",
+            "region_id": "region",
+        }
+    )
     filtered_lib = filtered_lib.fillna(0)
-    filtered_lib.to_sql("living_income_benchmark", con=engine, if_exists="append", index=False)
+    for index, row in filtered_lib.iterrows():
+        lib = LivingIncomeBenchmark(
+            id=row["id"],
+            country=row["country"],
+            region=row["region"],
+            household_size=row["household_size"],
+            year=row["year"],
+            source=row["source"],
+            lcu=row["lcu"],
+            usd=row["usd"],
+            eur=row["eur"],
+        )
+        session.add(lib)
+        session.commit()
+        session.flush()
+        session.refresh(lib)
     print("[DATABASE UPDATED]: Living Income Benchmark")
 
     ## CPI
@@ -36,18 +52,29 @@ def seeder_benchmark(session: Session, engine: create_engine):
     cpi = pd.read_csv(MASTER_DIR + "cpi.csv")
     # Filter rows where the list does not contain any string values
     filtered_cpi = cpi[cpi["country_id"] != cpi["country"]]
-    filtered_cpi.drop(columns=['country'], inplace=True)
-    filtered_cpi = filtered_cpi.rename(columns={
-        "country_id": "country",
-    })
+    filtered_cpi.drop(columns=["country"], inplace=True)
+    filtered_cpi = filtered_cpi.rename(
+        columns={
+            "country_id": "country",
+        }
+    )
     filtered_cpi = filtered_cpi.fillna(0)
-    filtered_cpi.to_sql("cpi", con=engine, if_exists="append", index=False)
+    for index, row in filtered_cpi.iterrows():
+        cpi = Cpi(
+            id=row["id"],
+            country=row["country"],
+            year=row["year"],
+            value=row["value"],
+        )
+        session.add(cpi)
+        session.commit()
+        session.flush()
+        session.refresh(cpi)
     print("[DATABASE UPDATED]: CPI")
-
-    generate_config_file()
+    session.close()
 
 
 if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
     session = SessionLocal()
-    seeder_benchmark(session=session, engine=engine)
+    seeder_benchmark(session=session)
