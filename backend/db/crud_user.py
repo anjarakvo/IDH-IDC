dfaaca116e027df7fa8eea3f93a9767cc7adb9b8
wayangalihpratama <wgprtm@gmail.com>
2023-11-03 13:14:01 +0800

    [#39] Send invitation email to invited user & register notif to BU admin

--- backend/db/crud_user.py
+++ backend/db/crud_user.py
@@ -1,11 +1,15 @@
 from uuid import uuid4
 from fastapi import HTTPException, status
 from typing import Optional, List
-from sqlalchemy import or_
+from sqlalchemy import or_, and_
 from sqlalchemy.orm import Session
 from models.user import (
-    User, UserDict, UserBase, UserUpdateBase, UserInvitation,
-    UserRole
+    User,
+    UserDict,
+    UserBase,
+    UserUpdateBase,
+    UserInvitation,
+    UserRole,
 )
 from models.user_case_access import UserCaseAccess
 from models.user_tag import UserTag
@@ -21,11 +25,7 @@ def add_user(
         password = payload.password.get_secret_value()
     except AttributeError:
         password = payload.password
-    role = (
-        payload.role
-        if invitation_id or payload.role
-        else UserRole.user
-    )
+    role = payload.role if invitation_id or payload.role else UserRole.user
     all_cases = 1 if payload.all_cases else 0
     if role in [UserRole.super_admin, UserRole.admin]:
         all_cases = 1
@@ -37,7 +37,7 @@ def add_user(
         role=role,
         all_cases=all_cases,
         is_active=1 if invitation_id else 0,
-        invitation_id=str(uuid4()) if invitation_id else None
+        invitation_id=str(uuid4()) if invitation_id else None,
     )
     if payload.tags:
         for tag in payload.tags:
@@ -46,7 +46,8 @@ def add_user(
     if payload.cases:
         for proj in payload.cases:
             case_access = UserCaseAccess(
-                case=proj["case"], permission=proj["permission"])
+                case=proj["case"], permission=proj["permission"]
+            )
             user.user_case_access.append(case_access)
     if payload.business_units:
         bu_role = (
@@ -56,8 +57,8 @@ def add_user(
         )
         for bu in payload.business_units:
             business_unit = UserBusinessUnit(
-                business_unit=bu["business_unit"],
-                role=bu_role)
+                business_unit=bu["business_unit"], role=bu_role
+            )
             user.user_business_units.append(business_unit)
     session.add(user)
     session.commit()
@@ -66,9 +67,7 @@ def add_user(
     return user
 
 
-def update_user(
-    session: Session, id: int, payload: UserUpdateBase
-) -> UserDict:
+def update_user(session: Session, id: int, payload: UserUpdateBase) -> UserDict:
     user = get_user_by_id(session=session, id=id)
     user.fullname = payload.fullname
     user.organisation = payload.organisation
@@ -87,8 +86,7 @@ def update_user(
         user.password = password
     if payload.tags:
         # delete prev user tags before update
-        prev_user_tags = session.query(UserTag).filter(
-            UserTag.user == user.id).all()
+        prev_user_tags = session.query(UserTag).filter(UserTag.user == user.id).all()
         for ut in prev_user_tags:
             session.delete(ut)
             session.commit()
@@ -98,17 +96,17 @@ def update_user(
             user.user_tags.append(user_tag)
     if payload.cases:
         # delete prev user cases before update
-        prev_user_cases = session.query(UserCaseAccess).filter(
-            UserCaseAccess.user == user.id).all()
+        prev_user_cases = (
+            session.query(UserCaseAccess).filter(UserCaseAccess.user == user.id).all()
+        )
         for uc in prev_user_cases:
             session.delete(uc)
             session.commit()
         # add new user case access
         for proj in payload.cases:
             case_access = UserCaseAccess(
-                user=user.id,
-                case=proj["case"],
-                permission=proj["permission"])
+                user=user.id, case=proj["case"], permission=proj["permission"]
+            )
             user.user_case_access.append(case_access)
     # Handle business units
     if payload.business_units:
@@ -118,17 +116,19 @@ def update_user(
             else UserBusinessUnitRole.member
         )
         # delete prev user business units before update
-        prev_user_bus = session.query(UserBusinessUnit).filter(
-            UserBusinessUnit.user == user.id).all()
+        prev_user_bus = (
+            session.query(UserBusinessUnit)
+            .filter(UserBusinessUnit.user == user.id)
+            .all()
+        )
         for bu in prev_user_bus:
             session.delete(bu)
             session.commit()
         # add new user business units
         for bu in payload.business_units:
             business_unit = UserBusinessUnit(
-                user=user.id,
-                role=bu_role,
-                business_unit=bu["business_unit"])
+                user=user.id, role=bu_role, business_unit=bu["business_unit"]
+            )
             user.user_business_units.append(business_unit)
     session.commit()
     session.flush()
@@ -153,8 +153,7 @@ def get_user_by_id(session: Session, id: int) -> UserDict:
     user = session.query(User).filter(User.id == id).first()
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"user {id} not found"
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"user {id} not found"
         )
     return user
 
@@ -173,7 +172,8 @@ def filter_user(
             or_(
                 User.fullname.ilike("%{}%".format(search.lower().strip())),
                 User.email.ilike("%{}%".format(search.lower().strip())),
-            ))
+            )
+        )
     if organisation:
         user = user.filter(User.organisation.in_([organisation]))
     if business_unit_users:
@@ -188,12 +188,15 @@ def get_all_user(
     organisation: Optional[List[int]] = None,
     business_unit_users: Optional[List[int]] = None,
     skip: int = 0,
-    limit: int = 10
+    limit: int = 10,
 ) -> List[UserDict]:
     user = filter_user(
-        session=session, search=search,
-        organisation=organisation, approved=approved,
-        business_unit_users=business_unit_users)
+        session=session,
+        search=search,
+        organisation=organisation,
+        approved=approved,
+        business_unit_users=business_unit_users,
+    )
     user = user.order_by(User.id.desc()).offset(skip).limit(limit).all()
     return user
 
@@ -206,9 +209,12 @@ def count(
     business_unit_users: Optional[List[int]] = None,
 ) -> int:
     user = filter_user(
-        session=session, search=search,
-        organisation=organisation, approved=approved,
-        business_unit_users=business_unit_users)
+        session=session,
+        search=search,
+        organisation=organisation,
+        approved=approved,
+        business_unit_users=business_unit_users,
+    )
     return user.count()
 
 
@@ -220,17 +226,14 @@ def delete_user(session: Session, id: int):
 
 
 def get_invitation(session: Session, invitation_id: str) -> UserInvitation:
-    user = session.query(User).filter(
-        User.invitation_id == invitation_id).first()
+    user = session.query(User).filter(User.invitation_id == invitation_id).first()
     if not user:
         return None
     return user
 
 
 def accept_invitation(
-    session: Session,
-    invitation_id: str,
-    password=str
+    session: Session, invitation_id: str, password=str
 ) -> UserInvitation:
     user = get_invitation(session=session, invitation_id=invitation_id)
     user.password = password
@@ -244,7 +247,16 @@ def accept_invitation(
 
 def find_same_business_unit(session: Session, user_id: int):
     return (
-        session.query(UserBusinessUnit)
-        .filter(UserBusinessUnit.user == user_id)
+        session.query(UserBusinessUnit).filter(UserBusinessUnit.user == user_id).all()
+    )
+
+
+def find_business_unit_admin(session: Session, user_id: int):
+    business_units = find_same_business_unit(session=session, user_id=user_id)
+    user_ids = [bu.user for bu in business_units]
+    admins = (
+        session.query(User)
+        .filter(and_(User.id.in_(user_ids), User.role == UserRole.admin))
         .all()
     )
+    return admins
