d75e39784a3718212214493f7a745d2514c6705d
dedenbangkit <mail@dedenbangkit.com>
2023-11-09 16:59:41 +0700

    [#82] Init scenario modeling page

--- frontend/src/pages/cases/components/DashboardScenarioModeling.js
+++ frontend/src/pages/cases/components/DashboardScenarioModeling.js
@@ -1,10 +1,44 @@
-import React from "react";
-import { Row, Col } from "antd";
+import React, { useState } from "react";
+import { Row, Col, Alert, Card } from "antd";
 
 const DashboardScenarioModeling = () => {
+  const [scenario, setScenario] = useState(1);
   return (
-    <Row gutter={[8, 8]}>
-      <Col>Scenario Modeling</Col>
+    <Row id="scenario-modeling">
+      <Col span={24}>
+        <Alert
+          message="On this page you can define and save up to three scenarios."
+          type="success"
+          className="alert-box"
+        />
+      </Col>
+      <Col span={24}>
+        <Card className="income-driver-dashboard">
+          <Card.Grid
+            style={{
+              width: "100%",
+            }}
+            hoverable={false}
+          >
+            <Row
+              className="income-driver-content"
+              align="middle"
+              justify="space-evenly"
+              gutter={[8, 8]}
+            >
+              <Col span={12}>
+                <h2>New Scenario</h2>
+                <p>Scenario Description</p>
+              </Col>
+              <Col span={12}>
+                <p>
+                  <b>Scenario 1</b>
+                </p>
+              </Col>
+            </Row>
+          </Card.Grid>
+        </Card>
+      </Col>
     </Row>
   );
 };
