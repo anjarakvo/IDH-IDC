711d5261db4eb7869cc59951335527a63cb7ab1c
wayangalihpratama <wgprtm@gmail.com>
2023-12-04 18:29:31 +0800

    [#138] Move framework drivers to component

--- frontend/src/pages/landing/components/index.js
+++ frontend/src/pages/landing/components/index.js
@@ -2,3 +2,4 @@ export { default as GetStarted } from "./GetStarted";
 export { default as ExploreStudies } from "./ExploreStudies";
 export { default as FooterDisclaimer } from "./FooterDisclaimer";
 export { default as Jumbotron } from "./Jumbotron";
+export { default as FrameworkDrivers } from "./FrameworkDrivers";
