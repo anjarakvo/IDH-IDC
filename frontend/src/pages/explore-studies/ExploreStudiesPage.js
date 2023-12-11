2644f89cd164fb5065daa724e2202715eb39803f
wayangalihpratama <wgprtm@gmail.com>
2023-12-11 15:42:05 +0800

    [#151] Map n map filter layout, styles

--- frontend/src/pages/explore-studies/ExploreStudiesPage.js
+++ frontend/src/pages/explore-studies/ExploreStudiesPage.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useMemo } from "react";
 import "./explore-studies-page.scss";
 import { ContentLayout } from "../../components/layout";
-import { Row, Col, Alert, Button } from "antd";
+import { Row, Col, Alert, Button, Card, Select, Input } from "antd";
+import { UserState, UIState } from "../../store";
+import { adminRole } from "../../store/static";
+import { SearchOutlined } from "@ant-design/icons";
+
+const selectProps = {
+  showSearch: true,
+  allowClear: true,
+  optionFilterProp: "label",
+  style: {
+    width: "100%",
+  },
+};
 
 const ExploreStudiesPage = () => {
+  const userRole = UserState.useState((s) => s.role);
+  const tagOptions = UIState.useState((s) => s.tagOptions);
+
+  const countryOptions = window.master.countries;
+
+  const isAdmin = useMemo(() => adminRole.includes(userRole), [userRole]);
+
   return (
     <ContentLayout
       breadcrumbItems={[
@@ -12,7 +31,7 @@ const ExploreStudiesPage = () => {
       ]}
       wrapperId="explore-studies-page"
     >
-      <Row gutter={[16, 16]} className="explore-content-wrapper">
+      <Row gutter={[24, 24]} className="explore-content-wrapper">
         <Col span={24}>
           <Alert
             className="explore-info-wrapper"
@@ -26,12 +45,62 @@ const ExploreStudiesPage = () => {
                   Here, you can access valuable insights into feasible levels of
                   income drivers for your selected country and sector.
                 </p>
-                <Button className="button button-green-fill">
-                  Create a new Cases
-                </Button>
+                {isAdmin ? (
+                  <Button className="button button-green-fill">
+                    Create a new Cases
+                  </Button>
+                ) : null}
               </div>
             }
           />
+        </Col>
+        <Col span={24}>
+          <Card title="Cases" className="info-card-wrapper">
+            <Row gutter={[16, 16]} className="explore-filter-wrapper">
+              <Col span={24}>
+                <Row gutter={[16, 16]} justify="space-between" align="bottom">
+                  <Col span={8}>
+                    <div className="filter-label">Country</div>
+                    <Select
+                      {...selectProps}
+                      options={countryOptions}
+                      placeholder="Select a Country"
+                    />
+                  </Col>
+                  <Col span={8}>
+                    <div className="filter-label">Map View</div>
+                    <Select
+                      {...selectProps}
+                      options={[]}
+                      placeholder="Select map view"
+                    />
+                  </Col>
+                  <Col span={8}>
+                    <Input prefix={<SearchOutlined />} placeholder="Search" />
+                  </Col>
+                </Row>
+              </Col>
+              <Col span={24}>
+                <Row gutter={[16, 16]} justify="space-between" align="bottom">
+                  <Col span={22}>
+                    <div className="filter-label">Tags</div>
+                    <Select
+                      {...selectProps}
+                      mode="multiple"
+                      options={tagOptions}
+                      placeholder="Select tags"
+                    />
+                  </Col>
+                  <Col span={2}>
+                    <Button className="search-button">Search</Button>
+                  </Col>
+                </Row>
+              </Col>
+              <Col span={24}>
+                <div className="map-container"></div>
+              </Col>
+            </Row>
+          </Card>
         </Col>
       </Row>
     </ContentLayout>
