c69c4cdd2db916ed890df29f0303caeb5acae0d1
wayangalihpratama <wgprtm@gmail.com>
2023-12-11 13:36:49 +0800

    [#151] Create explore case page info

--- frontend/src/pages/explore-studies/ExploreStudiesPage.js
+++ frontend/src/pages/explore-studies/ExploreStudiesPage.js
@@ -1,8 +1,41 @@
 import React from "react";
 import "./explore-studies-page.scss";
+import { ContentLayout } from "../../components/layout";
+import { Row, Col, Alert, Button } from "antd";
 
 const ExploreStudiesPage = () => {
-  return <div>ExploreStudiesPage</div>;
+  return (
+    <ContentLayout
+      breadcrumbItems={[
+        { title: "Home", href: "/welcome" },
+        { title: "Explore Studies" },
+      ]}
+      wrapperId="explore-studies-page"
+    >
+      <Row gutter={[16, 16]} className="explore-content-wrapper">
+        <Col span={24}>
+          <Alert
+            className="explore-info-wrapper"
+            type="success"
+            message={
+              <div className="explore-info-content">
+                <h2>Explore Studies for Insights</h2>
+                <p>
+                  To make the data entry process more informed and efficient, we
+                  recommend visiting the &quot;Explore Studies&quot; section.
+                  Here, you can access valuable insights into feasible levels of
+                  income drivers for your selected country and sector.
+                </p>
+                <Button className="button button-green-fill">
+                  Create a new Cases
+                </Button>
+              </div>
+            }
+          />
+        </Col>
+      </Row>
+    </ContentLayout>
+  );
 };
 
 export default ExploreStudiesPage;
