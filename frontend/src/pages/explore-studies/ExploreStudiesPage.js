a55da7697aea03d6f0304283dd4cb84110698e77
wayangalihpratama <wgprtm@gmail.com>
2023-12-11 13:11:11 +0800

    [#151] Init explore studies page

---
+++ frontend/src/pages/explore-studies/ExploreStudiesPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./explore-studies-page.scss";
+
+const ExploreStudiesPage = () => {
+  return <div>ExploreStudiesPage</div>;
+};
+
+export default ExploreStudiesPage;
