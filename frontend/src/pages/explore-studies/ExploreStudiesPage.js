32952c449d48134dac3727e0420c2a182d8c0457
wayangalihpratama <wgprtm@gmail.com>
2023-12-18 10:08:24 +0800

    [#156] Allow filter on explore studies page

--- frontend/src/pages/explore-studies/ExploreStudiesPage.js
+++ frontend/src/pages/explore-studies/ExploreStudiesPage.js
@@ -12,8 +12,9 @@ import {
   Input,
   Table,
   message,
+  Form,
 } from "antd";
-import { UserState, UIState } from "../../store";
+import { UserState } from "../../store";
 import { adminRole } from "../../store/static";
 import { SearchOutlined, EditOutlined } from "@ant-design/icons";
 import { upperFirst, isEmpty } from "lodash";
@@ -101,6 +102,29 @@ const referenceDataExpand = [
   },
 ];
 
+const driverOptions = [
+  {
+    label: "Area",
+    value: "area",
+  },
+  {
+    label: "Volume",
+    value: "volume",
+  },
+  {
+    label: "Price",
+    value: "price",
+  },
+  {
+    label: "Cost of Production",
+    value: "cost_of_production",
+  },
+  {
+    label: "Diversified Income",
+    value: "diversified_income",
+  },
+];
+
 const perPage = 10;
 const defData = {
   current: 1,
@@ -110,8 +134,8 @@ const defData = {
 };
 
 const ExploreStudiesPage = () => {
+  const [form] = Form.useForm();
   const userRole = UserState.useState((s) => s.role);
-  const tagOptions = UIState.useState((s) => s.tagOptions);
 
   const [open, setOpen] = useState(false);
   const [messageApi, contextHolder] = message.useMessage();
@@ -122,6 +146,12 @@ const ExploreStudiesPage = () => {
   const [expandedData, setExpandedData] = useState([]);
 
   const countryOptions = window.master.countries;
+  const commodityOptions = window?.master?.commodity_categories?.flatMap((ct) =>
+    ct.commodities.map((c) => ({
+      label: c.name,
+      value: c.id,
+    }))
+  );
 
   const isAdmin = useMemo(() => adminRole.includes(userRole), [userRole]);
 
@@ -172,25 +202,40 @@ const ExploreStudiesPage = () => {
     },
   ];
 
-  const fetchReferenceData = useCallback(() => {
-    setLoading(true);
-    let url = `reference_data?page=${currentPage}&limit=${perPage}`;
-    api
-      .get(url)
-      .then((res) => {
-        setData(res.data);
-      })
-      .catch((e) => {
-        console.error(e.response);
-        const { status } = e.response;
-        if (status === 404) {
-          setData(defData);
-        }
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, [currentPage]);
+  const fetchReferenceData = useCallback(
+    (country, commodity, driver, source) => {
+      setLoading(true);
+      let url = `reference_data?page=${currentPage}&limit=${perPage}`;
+      if (country) {
+        url = `${url}&country=${country}`;
+      }
+      if (commodity) {
+        url = `${url}&commodity=${commodity}`;
+      }
+      if (driver) {
+        url = `${url}&driver=${driver}`;
+      }
+      if (source) {
+        url = `${url}&source=${source}`;
+      }
+      api
+        .get(url)
+        .then((res) => {
+          setData(res.data);
+        })
+        .catch((e) => {
+          console.error(e.response);
+          const { status } = e.response;
+          if (status === 404) {
+            setData(defData);
+          }
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    },
+    [currentPage]
+  );
 
   const fetchReferenceDataDetail = (record) => {
     if (isEmpty(expandedData)) {
@@ -222,6 +267,11 @@ const ExploreStudiesPage = () => {
   useEffect(() => {
     fetchReferenceData();
   }, [fetchReferenceData, currentPage]);
+
+  const onFilter = (values) => {
+    const { country, commodity, driver, source } = values;
+    fetchReferenceData(country, commodity, driver, source);
+  };
 
   const onSave = ({ payload, setConfirmLoading, resetFields }) => {
     setConfirmLoading(true);
@@ -287,50 +337,85 @@ const ExploreStudiesPage = () => {
 
         <Col span={24}>
           <Card title="Cases" className="info-card-wrapper">
-            <Row gutter={[16, 16]} className="explore-filter-wrapper">
-              <Col span={24}>
-                <Row gutter={[16, 16]} justify="space-between" align="bottom">
-                  <Col span={8}>
-                    <div className="filter-label">Country</div>
-                    <Select
-                      {...selectProps}
-                      options={countryOptions}
-                      placeholder="Select a Country"
-                    />
-                  </Col>
-                  <Col span={8}>
-                    <div className="filter-label">Map View</div>
-                    <Select
-                      {...selectProps}
-                      options={[]}
-                      placeholder="Select map view"
-                    />
-                  </Col>
-                  <Col span={8}>
-                    <Input prefix={<SearchOutlined />} placeholder="Search" />
-                  </Col>
-                </Row>
-              </Col>
-              <Col span={24}>
-                <Row gutter={[16, 16]} justify="space-between" align="bottom">
-                  <Col span={22}>
-                    <div className="filter-label">Tags</div>
-                    <Select
-                      {...selectProps}
-                      mode="multiple"
-                      options={tagOptions}
-                      placeholder="Select tags"
-                    />
-                  </Col>
-                  <Col span={2}>
-                    <Button className="search-button">Search</Button>
-                  </Col>
-                </Row>
-              </Col>
-              <Col span={24}>
-                <div className="map-container"></div>
-              </Col>
-            </Row>
+            <Form
+              form={form}
+              name="filter-form"
+              className="filter-form-container"
+              layout="vertical"
+              // initialValues={initValues}
+              onFinish={onFilter}
+            >
+              <Row gutter={[16, 16]} className="explore-filter-wrapper">
+                <Col span={24}>
+                  <Row gutter={[16, 16]} justify="space-between" align="bottom">
+                    <Col span={12}>
+                      <div className="filter-label">Country</div>
+                      <Form.Item name="country" noStyle>
+                        <Select
+                          {...selectProps}
+                          options={countryOptions}
+                          placeholder="Select Country"
+                        />
+                      </Form.Item>
+                    </Col>
+                    <Col span={12}>
+                      <div className="filter-label">Commodity</div>
+                      <Form.Item name="commodity" noStyle>
+                        <Select
+                          {...selectProps}
+                          options={commodityOptions}
+                          placeholder="Select Commodity"
+                        />
+                      </Form.Item>
+                    </Col>
+                  </Row>
+                </Col>
+                <Col span={24}>
+                  <Row gutter={[16, 16]} justify="space-between" align="bottom">
+                    <Col span={12}>
+                      <div className="filter-label">Source</div>
+                      <Form.Item name="source" noStyle>
+                        <Input
+                          prefix={<SearchOutlined />}
+                          placeholder="Search by Source"
+                        />
+                      </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                      <div className="filter-label">Drivers</div>
+                      <Form.Item name="driver" noStyle>
+                        <Select
+                          {...selectProps}
+                          options={driverOptions}
+                          placeholder="Select Driver"
+                        />
+                      </Form.Item>
+                    </Col>
+                    <Col span={2}>
+                      <Form.Item noStyle>
+                        <Button className="search-button" htmlType="submit">
+                          Search
+                        </Button>
+                      </Form.Item>
+                    </Col>
+                    <Col span={2}>
+                      <Button
+                        className="clear-button"
+                        onClick={() => {
+                          form.resetFields();
+                          fetchReferenceData();
+                        }}
+                      >
+                        Clear
+                      </Button>
+                    </Col>
+                  </Row>
+                </Col>
+                <Col span={24}>
+                  <div className="map-container"></div>
+                </Col>
+              </Row>
+            </Form>
           </Card>
         </Col>
 
