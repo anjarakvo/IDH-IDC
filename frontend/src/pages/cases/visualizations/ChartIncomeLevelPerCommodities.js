029d063b72ab7b127b8229735533296069705f1d
wayangalihpratama <wgprtm@gmail.com>
2023-12-19 17:28:09 +0800

    [#169] Update chart colors

--- frontend/src/pages/cases/visualizations/ChartIncomeLevelPerCommodities.js
+++ frontend/src/pages/cases/visualizations/ChartIncomeLevelPerCommodities.js
@@ -4,7 +4,7 @@ import { SegmentSelector } from ".";
 import { uniqBy, capitalize, sum } from "lodash";
 import Chart from "../../../components/chart";
 
-const colors = ["#0098FF", "#FFC505", "#47D985", "#FF5D00", "#00625F"];
+const colors = ["#00625F", "#47D985", "#82B2B2"];
 const legendColors = ["#00625F", "#47D985", "#82B2B2"];
 
 const ChartIncomeLevelPerCommodities = ({ dashboardData, currentCase }) => {
